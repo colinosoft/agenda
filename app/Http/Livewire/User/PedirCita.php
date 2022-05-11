874597f86964ab2ab23d907a1ffd64db354a8bf9
colinosoft <colinosoft@hotmail.com>
2022-05-11 16:28:20 +0200

    Livewiew de pedir cita inicial

---
+++ app/Http/Livewire/User/PedirCita.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class PedirCita extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.pedir-cita');
+    }
+}
---
+++ resources/views/livewire/user/pedir-cita.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-4">
+                <form>
+                    <div class="mb-3">
+                        <label for="cita" class="form-label fs-2 fw-bold">Selecciones el tratamiento</label>
+                        <select class="form-select" name="servicio" id="servicio">
+
+                        </select>
+                    </div>
+                    <div class="d-grid gap-2">
+                    <button type="submit" id="confirmar" class="btn btn-primary">Confirmar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
